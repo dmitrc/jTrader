5bd5360c21580d7abfd92b210f89c92c79002c49
dmitryfd <fd@flexibleblog.ru>
2013-10-27 21:13:01 +0100

    badass index.html (and bootstrap and all the other stuff)

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+$(window).scroll(function () {
+
+});
+
+$(window).resize(function () { 
+   $('body').css('padding-top', parseInt($('#main-navbar').css("height")));
+
+});
+
+$(window).load(function () { 
+   $('body').css('padding-top', parseInt($('#main-navbar').css("height")));     
+
+}); 
