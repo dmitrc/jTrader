7646b7109a6f3a686646a114dbd9942c71e813a9
dmitryfd <fd@flexibleblog.ru>
2013-12-03 16:43:11 +0100

    user changes

--- inc/user-model.php
+++ inc/user-model.php
@@ -4,22 +4,16 @@
 
 	class User {
 		var $eid;
-		var $employeetype;
 		var $account;
-		var $attributes;
 		var $fname;
 		var $lname;
-		var $displayname;
-		var $name;
 		var $cn;
 		var $college;
-		var $majorinfo;
-		var $majorlong;
+		var $major;
 		var $country;
 		var $email;
 		var $room; // to be parsed
 		var $phone;
-		var $description;
 		var $www;
 
 		function writeToDb() {
@@ -32,7 +26,7 @@
 
         $conn = ldap_connect("jacobs.jacobs-university.de",389);
         if (!ldap_bind($conn,$username."@jacobs.jacobs-university.de",$password)) {
-            echo "no";
+            echo "false";
             return false;
         }
 
@@ -40,7 +34,7 @@
         $r = ldap_get_entries($conn, $search);
 
         if (count($r) == 0) {
-            echo "no";
+            echo "false";
             return false;
         }
 
@@ -48,22 +42,15 @@
 
 
         $user->eid = htmlentities(utf8_encode($r[0]['employeeid'][0]),ENT_COMPAT,'utf-8');
-        $user->employeetype = htmlentities(utf8_encode($r[0]['company'][0]),ENT_COMPAT,'utf-8');
         $user->account = htmlentities(utf8_encode($r[0]['samaccountname'][0]),ENT_COMPAT,'utf-8');
-        $user->attributes = htmlentities(utf8_encode($r[0]['employeetype'][0]),ENT_COMPAT,'utf-8');
         $user->fname = htmlentities(utf8_encode($r[0]['givenname'][0]),ENT_COMPAT,'utf-8');
         $user->lname = htmlentities(utf8_encode($r[0]['sn'][0]),ENT_COMPAT,'utf-8');
-        $user->displayname = htmlentities(utf8_encode($r[0]['displayname'][0]),ENT_COMPAT,'utf-8');
-        $user->name = htmlentities(utf8_encode($r[0]['name'][0]),ENT_COMPAT,'utf-8');
         $user->cn = htmlentities(utf8_encode($r[0]['cn'][0]),ENT_COMPAT,'utf-8');
         $user->college = htmlentities(utf8_encode($r[0]['houseidentifier'][0]),ENT_COMPAT,'utf-8');
-        $user->majorinfo = htmlentities(utf8_encode($r[0]['extensionattribute2'][0]),ENT_COMPAT,'utf-8');
-        $user->majorlong = htmlentities(utf8_encode($r[0]['extensionattribute3'][0]),ENT_COMPAT,'utf-8');
+        $user->major = htmlentities(utf8_encode($r[0]['extensionattribute3'][0]),ENT_COMPAT,'utf-8');
         $user->country = htmlentities(utf8_encode($r[0]['extensionattribute5'][0]),ENT_COMPAT,'utf-8');
         $user->email = htmlentities(utf8_encode($r[0]['mail'][0]),ENT_COMPAT,'utf-8');
         $user->phone = htmlentities(utf8_encode($r[0]['telephonenumber'][0]),ENT_COMPAT,'utf-8');
-        $user->description = htmlentities(utf8_encode($r[0]['description'][0]),ENT_COMPAT,'utf-8');
-        $user->www = htmlentities(utf8_encode($r[0]['wwwhomepage'][0]),ENT_COMPAT,'utf-8');
         $user->room = " ";
 
         $_SESSION["user"] = $user;
@@ -87,7 +74,7 @@
       }
       else
       {
-        echo "<no>";
+        echo "false";
       }
     }
 
