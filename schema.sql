9e3d0e2e578b9b7f7f1a26891d2ae08906f06c6b
Dmitrii Cucleschin <dmitrii.cucleschin@gmail.com>
2014-05-22 20:35:03 +0200

    schema turned out to be broken. wow, took us months to notice...

--- schema.sql
+++ schema.sql
@@ -1,29 +1,23 @@
--- Databases & Web Applications
--- Homework #3, Assignment #2.
-
--- Author: Dmitrii Cucleschin
--- Author: Nikolche Kolev
--- Author: Andrei Giurgiu
-
-
--- --------------------------------------------------------
--- Define table structures
--- --------------------------------------------------------
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 --
 -- Table structure for table `Admins`
 --
+
 CREATE TABLE IF NOT EXISTS `Admins` (
   `aid` int(11) NOT NULL AUTO_INCREMENT,
   `eid` int(11) NOT NULL,
   `clearanceLevel` int(11) NOT NULL,
   PRIMARY KEY (`aid`),
   UNIQUE KEY `FK_admin_eid` (`eid`)
-);
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `AuctionBids`
 --
+
 CREATE TABLE IF NOT EXISTS `AuctionBids` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `buyerid` int(11) NOT NULL,
@@ -32,11 +26,14 @@ CREATE TABLE IF NOT EXISTS `AuctionBids` (
   PRIMARY KEY (`id`),
   KEY `FK_auctionbids_buyerid` (`buyerid`),
   KEY `FK_auctionbids_offerid` (`offerid`)
-);
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `Auctions`
 --
+
 CREATE TABLE IF NOT EXISTS `Auctions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `offerid` int(11) NOT NULL,
@@ -46,11 +43,14 @@ CREATE TABLE IF NOT EXISTS `Auctions` (
   PRIMARY KEY (`id`),
   KEY `FK_auctions_offerid` (`offerid`),
   KEY `FK_auctions_highestbid` (`currentHighestBid`)
-);
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `Categories`
 --
+
 CREATE TABLE IF NOT EXISTS `Categories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `type` varchar(48) NOT NULL,
@@ -59,11 +59,14 @@ CREATE TABLE IF NOT EXISTS `Categories` (
   PRIMARY KEY (`id`),
   KEY `typeIndex` (`type`),
   KEY `subtypeIndex` (`subtype`)
-);
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=50 ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `FixedPriceBids`
 --
+
 CREATE TABLE IF NOT EXISTS `FixedPriceBids` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `buyerid` int(11) NOT NULL,
@@ -72,22 +75,28 @@ CREATE TABLE IF NOT EXISTS `FixedPriceBids` (
   PRIMARY KEY (`id`),
   KEY `FK_fixedpricebids_fpofferid` (`fixedPriceOfferID`),
   KEY `FK_fixedpricebids_buyerid` (`buyerid`)
-);
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=27 ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `FixedPriceOffers`
 --
+
 CREATE TABLE IF NOT EXISTS `FixedPriceOffers` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `offerid` int(11) NOT NULL,
   `price` decimal(10,2) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_fixedpriceoffers_offerid` (`offerid`)
-);
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=27 ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `OfferRequests`
 --
+
 CREATE TABLE IF NOT EXISTS `OfferRequests` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `userid` int(11) NOT NULL,
@@ -97,11 +106,14 @@ CREATE TABLE IF NOT EXISTS `OfferRequests` (
   PRIMARY KEY (`id`),
   KEY `FK_offerrequest_userid` (`userid`),
   KEY `FK_offerrequest_catid` (`catid`)
-);
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `Offers`
 --
+
 CREATE TABLE IF NOT EXISTS `Offers` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `userid` int(11) NOT NULL,
@@ -109,14 +121,18 @@ CREATE TABLE IF NOT EXISTS `Offers` (
   `name` varchar(128) NOT NULL,
   `description` text NOT NULL,
   `postTime` datetime NOT NULL,
+  `picturePath` varchar(128) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_offers_userid` (`userid`),
   KEY `FK_offers_catid` (`catid`)
-);
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=34 ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `Subscriptions`
 --
+
 CREATE TABLE IF NOT EXISTS `Subscriptions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `userid` int(11) NOT NULL,
@@ -124,54 +140,60 @@ CREATE TABLE IF NOT EXISTS `Subscriptions` (
   PRIMARY KEY (`id`),
   KEY `FK_subscriptions_userid` (`userid`),
   KEY `FK_subscriptions_catid` (`catid`)
-);
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `Users`
 --
+
 CREATE TABLE IF NOT EXISTS `Users` (
-  `eid` int(11) NOT NULL AUTO_INCREMENT,
+  `eid` int(16) NOT NULL,
+  `name` varchar(32) NOT NULL,
   `fname` varchar(32) NOT NULL,
   `lname` varchar(32) NOT NULL,
   `roomPhone` int(4) NOT NULL,
   `roomNumber` varchar(10) NOT NULL,
-  `college` char(1) NOT NULL,
+  `college` char(32) NOT NULL,
   `email` varchar(64) NOT NULL,
-  `mobile` varchar(32) NOT NULL,
   `rating` float NOT NULL DEFAULT '0',
   PRIMARY KEY (`eid`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 
 -- --------------------------------------------------------
--- Define constraints for the tables
--- --------------------------------------------------------
+
+--
+-- Constraints for dumped tables
+--
 
 --
 -- Constraints for table `Admins`
 --
 ALTER TABLE `Admins`
-  ADD CONSTRAINT `admins_ibfk_1` FOREIGN KEY (`eid`) REFERENCES `Users` (`eid`) ON DELETE RESTRICT ON UPDATE CASCADE;
+  ADD CONSTRAINT `admins_ibfk_1` FOREIGN KEY (`eid`) REFERENCES `Users` (`eid`) ON UPDATE CASCADE;
 
 --
 -- Constraints for table `AuctionBids`
 --
 ALTER TABLE `AuctionBids`
-  ADD CONSTRAINT `auctionbids_ibfk_2` FOREIGN KEY (`offerid`) REFERENCES `Offers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `auctionbids_ibfk_1` FOREIGN KEY (`buyerid`) REFERENCES `Users` (`eid`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `auctionbids_ibfk_1` FOREIGN KEY (`buyerid`) REFERENCES `Users` (`eid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `auctionbids_ibfk_2` FOREIGN KEY (`offerid`) REFERENCES `Offers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `Auctions`
 --
 ALTER TABLE `Auctions`
-  ADD CONSTRAINT `auctions_ibfk_2` FOREIGN KEY (`currentHighestBid`) REFERENCES `AuctionBids` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `auctions_ibfk_1` FOREIGN KEY (`offerid`) REFERENCES `Offers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `auctions_ibfk_1` FOREIGN KEY (`offerid`) REFERENCES `Offers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `auctions_ibfk_2` FOREIGN KEY (`currentHighestBid`) REFERENCES `AuctionBids` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `FixedPriceBids`
 --
 ALTER TABLE `FixedPriceBids`
-  ADD CONSTRAINT `fixedpricebids_ibfk_2` FOREIGN KEY (`fixedPriceOfferID`) REFERENCES `FixedPriceOffers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fixedpricebids_ibfk_1` FOREIGN KEY (`buyerid`) REFERENCES `Users` (`eid`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `fixedpricebids_ibfk_1` FOREIGN KEY (`buyerid`) REFERENCES `Users` (`eid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fixedpricebids_ibfk_2` FOREIGN KEY (`fixedPriceOfferID`) REFERENCES `FixedPriceOffers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `FixedPriceOffers`
@@ -183,19 +205,19 @@ ALTER TABLE `FixedPriceOffers`
 -- Constraints for table `OfferRequests`
 --
 ALTER TABLE `OfferRequests`
-  ADD CONSTRAINT `offerrequests_ibfk_2` FOREIGN KEY (`catid`) REFERENCES `Categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `offerrequests_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `Users` (`eid`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `offerrequests_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `Users` (`eid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `offerrequests_ibfk_2` FOREIGN KEY (`catid`) REFERENCES `Categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `Offers`
 --
 ALTER TABLE `Offers`
-  ADD CONSTRAINT `offers_ibfk_2` FOREIGN KEY (`catid`) REFERENCES `Categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `offers_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `Users` (`eid`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `offers_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `Users` (`eid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `offers_ibfk_2` FOREIGN KEY (`catid`) REFERENCES `Categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `Subscriptions`
 --
 ALTER TABLE `Subscriptions`
-  ADD CONSTRAINT `subscriptions_ibfk_2` FOREIGN KEY (`catid`) REFERENCES `Categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `subscriptions_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `Users` (`eid`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `subscriptions_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `Users` (`eid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `subscriptions_ibfk_2` FOREIGN KEY (`catid`) REFERENCES `Categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
